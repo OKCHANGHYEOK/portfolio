2d814d8eaadccb6b36116850400bd20d9e4db862
OCH <dkdldy2@naver.com>
2024-04-23 21:07:39 +0900

    포트폴리오 사이트 제작 1일차 커밋

---
+++ src/test/java/com/itbank/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itbank.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
